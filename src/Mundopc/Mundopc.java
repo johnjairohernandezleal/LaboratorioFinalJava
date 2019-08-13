f2ec21eb29b2587907f280b6c3aee38db8bd8882
unknown <john_0112@hotmail.com>
2019-08-13 02:02:25 -0500

    first commit

---
+++ src/Mundopc/Mundopc.java
@@ -0,0 +1,10 @@
+package Mundopc;
+import com.gm.mundopc.*;
+
+public class Mundopc {
+    public static void main(String[] args) {
+        // creacion computadora Toshiba
+        Monitor monitorToshi = new Monitor("Toshiba",13);
+        Teclado tecladoTosi = new Teclado("USB","Toshiba");
+    }
+}
